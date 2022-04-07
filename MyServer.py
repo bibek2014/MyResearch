01155f42e4beb323005e0a52384e8838ff94d7e7
BIBEKANANDA KUNDU <bibekananda.kundu@gmail.com>
2022-04-07 16:07:44 +0530

    Update MyServer.py

--- MyServer.py
+++ MyServer.py
@@ -1,6 +1,7 @@
 import os,zipfile
-from flask import Flask, render_template, send_file
-
+from flask import Flask, render_template, send_file, request, jsonify
+import base64
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -15,6 +16,29 @@ def GetStoredFiles():
 def index():
     Flnm=	GetStoredFiles()
     return render_template('index.html',Flnm=Flnm)
+
+
+
+audio_encoded = base64.b64encode
+@app.route('/api/add_message/<uuid>', methods=['GET', 'POST'])
+def add_message(uuid):
+    content = request.json
+    print(content['audioData'])
+    print(content['uid']) 	
+	
+    ans = base64.b64decode(bytes(content["audioData"], 'utf-8'))
+
+    print(type(ans)) #This is type bytes
+
+    now = datetime.now() # current date and time
+    today = now.strftime("%d-%m-%Y")
+    flname ="Data/"+content['uid']+"_"+today
+
+    with open(flname+".wav", "wb") as fh:
+        fh.write(ans)
+
+    return jsonify({"uuid":uuid})
+
 
 
 @app.route('/download_files')
